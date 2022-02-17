323425241735af30e665ae4c13271b82bb3587a0
ConnorEhrgood <ehrackdigitalarts@gmail.com>
2022-02-17 16:27:00 -0500

    Added megamd5; cleaned up var names in goget

--- MCGTools.py
+++ MCGTools.py
@@ -3,10 +3,10 @@ def goget(addr, output): #This function downloads files from HTTP destinations f
     import cursor
     cursor.hide()
     head = requests.head(addr)
-    ftype = head.headers['content-type'] #Gets file type and stores as string
-    fsize = int(head.headers['content-length']) #Gets file size (in bytes) and stores as int
+    file_type = head.headers['content-type'] #Gets file type and stores as string
+    file_size = int(head.headers['content-length']) #Gets file size (in bytes) and stores as int
     print(f'Downloading from {addr} as {output}')
-    print(f'File type: {ftype}  File Size: {fsize}')
+    print(f'File type: {file_type}  File Size: {file_size}')
     chunk_count = 0 #Counts how many chunks have been downloaded to calculate progress
     with requests.get(addr, stream=True) as r: #Gets in streaming mode to prevent entire file being copied to RAM
         r.raise_for_status() #Outputs debugging data
@@ -14,7 +14,7 @@ def goget(addr, output): #This function downloads files from HTTP destinations f
             for chunk in r.iter_content(chunk_size=8192): #Copies data chunk by chunk
                 f.write(chunk)
                 chunk_count += 1 #Increments chunck count
-                prog = int(((chunk_count * 8192) / fsize)*100) #Calculates progress percentage
+                prog = int(((chunk_count * 8192) / file_size)*100) #Calculates progress percentage
                 print(f'{prog}% Complete.', end = '\r',) #Prints progress percentage
     print("Download complete!")
     cursor.show()
@@ -27,6 +27,20 @@ def makelsdirs(path, dirsls): #This function builds directory trees based upon n
         if dirs: #Checks to see if any sub-directories have been specified
             subdirs = dirsls[dirs] #Parses the subdirectories into a new list to be passed into this function again
             makelsdirs(dir_path, subdirs) #Call to this function to make sub-directiories and any of their sub-directories...
+def megamd5(file): #This function generates the MD5 hash of an input file ans saves it to a .md5 file with the same name in the same directory as the input file
+    import os
+    import hashlib
+    file_name = os.path.split(file)[1]  #Creates a string of the input filename without the path; i.e. 'file.txt'
+    file_base = os.path.splitext(file_name)[0] #Creates a string of the base of the input filename without the extension; i.e. 'file'
+    file_path = os.path.split(file)[0] #Creates a string of the path to the input filename; i.e. '/home/directory/'
+    output_path = os.path.join(file_path, f'{file_base}.md5') #Creates the output path for the MD5 hash; i.e. '/home/directory/file.md5'
+    with open(file, 'rb') as f:
+        file_md5 = hashlib.md5()
+        while chunk := f.read(16384): #Computes MD5 hash in chunks to avoid loading entire file into ram
+            file_md5.update(chunk)
+    hexi_sum = file_md5.hexdigest() #Saves MD5 hash as string
+    with open(output_path, 'w') as o:
+        o.write(f'{hexi_sum} {file_name}') #Writes MD5 hash and file name into a text document in a format that md5sum can verify
 def DIRector(path, cFile): #This functions builds directory trees based upon a MCGTools YAML config file
     import yaml
     with open(cFile, 'r') as cOpen:
